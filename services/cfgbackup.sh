d41a8ba6e7167ea96c915c4e1e51340496945fb0
Perdyx <29049651+Perdyx@users.noreply.github.com>
2019-12-21 21:21:06 -0800

    Update configs, services

--- services/cfgbackup.sh
+++ services/cfgbackup.sh
@@ -9,35 +9,14 @@ int=300
 
 [ -d "$cfgdir" ] || git clone https://github.com/Perdyx/configs.git $home/.cfg
 
-function check() {
-    cfg=$1
-    s1=$(sha1sum $cfg)
-}
-
-function compare() {
-    if [ -f "$cfg" ]; then
-        s2=$(sha1sum $cfg)
-
-        if [ "$s1" != "$s2" ]; then
-            cp $cfg $1
-        fi
-    fi
-}
-
 while true; do
     dconf dump / > $cfgdir/dconf/$(cat /etc/*-release | grep "DISTRIB_ID" | sed 's/DISTRIB_ID=//')
 
-    check "$home/.gitconfig"
-    check "$home/.config/terminator/config"
-    check "$home/.vimrc"
-    check "$home/.xbindkeysrc"
-    check "$home/.zshrc"
+    cp $home/.gitconfig $cfgdir/git/.gitconfig
+    cp $home/.config/terminator/config $cfgdir/terminator/config
+    cp $home/.vimrc $cfgdir/vim/.vimrc
+    cp $home/.xbindkeysrc $cfgdir/xbindkeys/.xbindkeysrc
+    cp $home/.zshrc $cfgdir/zsh/.zshrc
 
     sleep $int
-
-    compare "$cfgdir/git/.gitconfig"
-    compare "$cfgdir/terminator/config"
-    compare "$cfgdir/vim/.vimrc"
-    compare "$cfgdir/xbindkeys/.xbindkeysrc"
-    compare "$cfgdir/zsh/.zshrc"
 done

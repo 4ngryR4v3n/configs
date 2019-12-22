bb64aa0609a8dd6ce66f596291151b1d79038881
Perdyx <29049651+Perdyx@users.noreply.github.com>
2019-12-21 22:52:52 -0800

    Restructure

--- post-install/ubuntu.sh
+++ post-install/ubuntu.sh
@@ -14,15 +14,11 @@ home=$(cd && pwd)
 cfgdir=".cfg"
 
 # Install miscellaneous packages
-sudo apt install -y aircrack-ng bleachbit gparted htop macchanger netdiscover net-tools nmap ranger transmission virtualbox virtualbox-guest-additions-iso wireshark
+sudo apt install -y aircrack-ng bleachbit gparted htop macchanger netdiscover net-tools nmap terminator transmission vim virtualbox virtualbox-guest-additions-iso wireshark xbindkeys xdotool
 sudo snap install --classic code
 sudo snap install discord
 sudo snap install spotify
 sudo snap install telegram-desktop
-
-# Install stock GNOME and reset GDM theme
-sudo apt install -y gnome-session
-sudo update-alternatives --config gdm3.css
 
 # Git
 sudo apt install -y git
@@ -33,41 +29,22 @@ else
 fi
 cp $home/$cfgdir/configs/git/.gitconfig $home/.gitconfig
 
-# Config backup service
-sudo chmod +x $home/$cfgdir/services/cfgbackup.sh
-sed -i "s/USER/$user/g" $home/$cfgdir/services/cfgbackup.service
-sudo cp $home/$cfgdir/services/cfgbackup.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable cfgbackup.service
-
-# Gnome
-sudo apt install -y dconf-editor gnome-tweak-tool fonts-firacode gnome-shell-extensions gnome-shell-extension-dashtodock papirus-icon-theme
+# Install and set up GNOME
+sudo apt install -y dconf-editor gnome-session gnome-tweak-tool fonts-firacode gnome-shell-extensions gnome-shell-extension-dashtodock papirus-icon-theme
+sudo update-alternatives --config gdm3.css
 sudo mkdir /usr/share/gnome-shell/extensions/inactive
 sudo mv /usr/share/gnome-shell/extensions/ubuntu-dock@ubuntu.com /usr/share/gnome-shell/extensions/inactive
-dconf load / < $home/$cfgdir/configs/dconf/Ubuntu
 
-# Terminator
-sudo apt install -y terminator
+# Set up configs
 mkdir $home/.config/terminator
 cp $home/$cfgdir/configs/terminator/config $home/.config/terminator/config
 sudo update-alternatives --config x-terminal-emulator
-
-# Vim
-sudo apt install -y vim
 cp $home/$cfgdir/configs/vim/.vimrc $home
+cp $home/$cfgdir/configs/xbindkeys/.xbindkeysrc $home
 
 # Ssh
 sudo apt install -y openssh-server
 sudo systemctl disable ssh
-
-# Macros
-sudo apt install -y xbindkeys xdotool
-cp $home/$cfgdir/configs/xbindkeys/.xbindkeysrc $home
-
-# Metasploit
-sudo apt install -y curl
-sudo sh -c "curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall"
-sudo rm -f msfinstall
 
 # Zsh
 sudo apt install -y curl zsh

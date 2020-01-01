8364b9e6fea5685ca58ba723c84eed0218e41c19
Perdyx <29049651+Perdyx@users.noreply.github.com>
2019-12-31 16:59:19 -0800

    `$commit_str`

--- backup.sh
+++ backup.sh
@@ -5,40 +5,48 @@ set -ex
 repo=$(pwd)
 home=$(cd ~/ && pwd)
 commit=()
+updated=False
 
 cfg_git=$home/.gitconfig
 if [ -f $cfg_git ]; then
     cp $cfg_git $repo/git
     commit+=("$cfg_git")
+    updated=True
 fi
 
 cfg_terminator=$home/.config/terminator/config
 if [ -f $cfg_terminator ]; then
     cp $cfg_terminator $repo/terminator
     commit+=("$cfg_terminator")
+    updated=True
 fi
 
 cfg_vim=$home/.vimrc
 if [ -f $cfg_vim ]; then
     cp $cfg_vim $repo/vim
     commit+=("$cfg_vim")
+    updated=True
 fi
 
 cfg_xbindkeys=$home/.xbindkeysrc
 if [ -f $cfg_xbindkeys ]; then
     cp $cfg_xbindkeys $repo/xbindkeys
     commit+=("$cfg_xbindkeys")
+    updated=True
 fi
 
 cfg_zsh=$home/.zshrc
 if [ -f $cfg_zsh ]; then
     cp $cfg_zsh $repo/zsh
     commit+=("$cfg_zsh")
+    updated=True
 fi
 
-commit_str=$(printf "%s\n"  "${commit[@]}")
+if [ "$updated" == True ]; then
+    commit_str=$(printf "Update\n%s\n"  "${commit[@]}")
 
-cd $repo
-git add .
-git commit -m '`$commit_str`'
-git push
+    cd $repo
+    git add .
+    git commit -m '`$commit_str`'
+    git push
+fi
